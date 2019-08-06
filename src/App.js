786f970c059a5ba173ea8fa34f259d1af0b53d26
Lee Gainer <piealacode@gmail.com>
2019-08-06 16:38:19 -0400

    Added style and html for several shapes

--- src/App.js
+++ src/App.js
@@ -5,10 +5,22 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <img src={logo} className="Logo Spin Spacing" alt="logo" />
-      <div className="Square Spacing Spin" alt="Blue Square"></div>
-      <div className="Rectangle Spacing Spin-Slow" alt="Red Rectangle"></div>
-      <div className="Circle Spacing Spin" alt="Green Circle"></div>
+      <img src={logo} className="Logo Spacing" alt="logo" />
+      <div className="Square Spacing" alt="Blue Square"></div>
+      <div className="Rectangle Spacing -Slow" alt="Red Rectangle"></div>
+      <div className="Circle Spacing" alt="Circle with Blue, Purple and Pink"></div>
+      <div className="Oval Spacing -Slow" alt="Mustard Yellow Oval"></div>
+      <div className="Triangle-Up Spacing" alt="Teal-blue Triangle pointing up"></div>
+      <div className="Triangle-Down Spacing" alt="Lime-green Triangle pointing down"></div>
+      <div className="Triangle-Left Spacing" alt="Dark Blue Triangle pointing left"></div>
+      <div className="Triangle-Right Spacing" alt="Mustard Yellow Triangle pointing right"></div>
+      <div className="Triangle-Topleft Spacing" alt="Pink Triangle"></div>
+      <div className="Triangle-Topright Spacing" alt="Dark Red Triangle"></div>
+      <div className="Triangle-Bottomleft Spacing" alt="Orange Triangle"></div>
+      <div className="Triangle-Bottomright Spacing" alt="Purple Triangle"></div>
+      <div className="Curved-Arrow Spacing" alt="A curved arrow"></div>
+      <div className="Trapezoid Spacing"></div>
+
     </div>
   );
 }
